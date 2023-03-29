3586aa44c45a249def207d41e760c1af685e5765
StrongJack123 <greatwisdom1223@gmail.com>
2023-03-29 12:55:53 +0900

    add error occured alert

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,6 +9,7 @@ import { AuthService } from "./auth.service";
 export class AuthComponent{
     isLoginMode: boolean = false; 
     isLoading: boolean = false; 
+    error: string = null; 
 
     constructor(private authService: AuthService) {}
 
@@ -33,6 +34,7 @@ export class AuthComponent{
                     this.isLoading = false;
                 }, error => {
                     console.log(error)
+                    this.error = "Error Occured" 
                     this.isLoading = false;
                 })
             }
