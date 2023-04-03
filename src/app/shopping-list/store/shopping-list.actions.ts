238f02b78764ca811cc514417fbfde727093a9cd
StrongJack123 <greatwisdom1223@gmail.com>
2023-04-03 16:47:04 +0900

    prepare && update - delete actions

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -4,6 +4,8 @@ import { Ingredients } from '../../shared/ingredients.component';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT'
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
@@ -17,4 +19,13 @@ export class AddIngredients implements Action {
   }
 }
 
-export type ShoppingListActions = AddIngredient | AddIngredients;
+export class UpdateIngredient implements Action {
+  readonly type = UPDATE_INGREDIENT
+  constructor( public payload: {index: number, ingredient: Ingredients}) {}
+}
+
+export class DeleteIngredient implements Action {
+  readonly type = DELETE_INGREDIENT;
+  constructor(public payload: number) {}
+}
+export type ShoppingListActions = AddIngredient | AddIngredients | UpdateIngredient | DeleteIngredient;
